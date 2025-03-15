5ed23c5e725774557e4b824d54e03f0cec8a5c7b
Shebie <sayedshabir00@gmail.com>
2025-03-16 02:47:31 +0500

    Add files via upload

---
+++ GameShpere/got.js
@@ -0,0 +1,20 @@
+const video = document.getElementById("gameVideo");
+const playButton = document.getElementById("playButton");
+const videoWrapper = document.querySelector(".video-wrapper");
+
+// Function to play video and hide button
+playButton.addEventListener("click", () => {
+    video.play(); 
+    hidePlayButton();
+});
+
+// Hide button when video starts playing
+video.addEventListener("play", hidePlayButton);
+
+// Hide button when user seeks the video
+video.addEventListener("seeking", hidePlayButton);
+
+// Function to hide play button
+function hidePlayButton() {
+    videoWrapper.classList.add("playing"); // Hide button
+}
